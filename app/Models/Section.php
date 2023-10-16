b48d001771d280339e755a8c3d0ecfea8b8b94e4
zbarzanooni@yahoo.com <zbarzanooni@yahoo.com>
2023-10-16 12:04:02 -0700

    filament test

---
+++ app/Models/Section.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Section extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'class_id'
+    ];
+    public function Class(){
+        return $this->belongsTo(Classes::class);
+    }
+}
